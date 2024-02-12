28c21e68d7240a5de0df05e49c63a678743c74eb
Juan S. López <dir.desarrollo@whitecloud.com.co>
2024-02-12 16:06:00 -0500

    refactor: remove comments

--- lib/features/pokemon/presentation/providers/selected_pokemon_item_provider.dart
+++ lib/features/pokemon/presentation/providers/selected_pokemon_item_provider.dart
@@ -1,18 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class SelectedPokemonItemProvider extends ChangeNotifier {
-//   int number;
-
-//   SelectedPokemonItemProvider({
-//     this.number = 0,
-//   });
-
-//   void changeNumber({required int newNumber}) {
-//     number = newNumber;
-//     notifyListeners();
-//   }
-// }
-
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'selected_pokemon_item_provider.g.dart';
--- lib/features/skeleton/providers/selected_page_provider.dart
+++ lib/features/skeleton/providers/selected_page_provider.dart
@@ -1,18 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class SelectedPageProvider extends ChangeNotifier {
-//   int selectedPage;
-
-//   SelectedPageProvider({
-//     this.selectedPage = 0,
-//   });
-
-//   void changePage(int newValue) {
-//     selectedPage = newValue;
-//     notifyListeners();
-//   }
-// }
-
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'selected_page_provider.g.dart';
